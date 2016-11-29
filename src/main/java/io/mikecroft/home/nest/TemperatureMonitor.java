8b30fa39c83a742496d126cbf6828e7430df4f6a
Mike Croft <backtotheoldhouse@gmail.com>
2016-11-29 22:14:49 +0000

    added some influxdb stuff that's super fragile and messy

--- src/main/java/io/mikecroft/home/nest/TemperatureMonitor.java
+++ src/main/java/io/mikecroft/home/nest/TemperatureMonitor.java
@@ -4,12 +4,18 @@ import com.bwssystems.nest.controller.Nest;
 import com.bwssystems.nest.controller.NestSession;
 import com.bwssystems.nest.controller.Thermostat;
 import com.bwssystems.nest.protocol.error.LoginException;
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.Point;
+import org.influxdb.dto.Query;
+import org.influxdb.dto.QueryResult;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by mike on 24/11/16.
@@ -55,13 +61,32 @@ public class TemperatureMonitor {
         Set<String> thermoNames = theNest.getThermostatNames(); /* list of thermostats in all structure */
         Thermostat thermo1 = theNest.getThermostat(thermoNameFromSet);
 
+        Double temp = thermo1.getSharedDetail().getCurrentTemperature();
+        Long humid = thermo1.getDeviceDetail().getCurrentHumidity();
         System.out.println("\n");
-        System.out.println("Humid: -- " + thermo1.getDeviceDetail().getCurrentHumidity());
-        System.out.println("Temp:  -- " + thermo1.getSharedDetail().getCurrentTemperature());
+        System.out.println("Humid: -- " + humid);
+        System.out.println("Temp:  -- " + temp);
         System.out.println("\n");
 
+
+        InfluxDB influxDB = InfluxDBFactory.connect("http://localhost:8086", "root", "root");
+        String dbName = "tempTest1";
+
+
+        influxDB.createDatabase(dbName);
+
+        // Flush every 2 points, at least every 5 seconds
+        influxDB.enableBatch(2, 5, TimeUnit.SECONDS);
+        Point point1 = Point.measurement("hallway")
+                .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+                .addField("temperature", temp)
+                .addField("humidity", humid)
+                .build();
+
+        influxDB.write(dbName, "autogen", point1);
+
         /*
-        TODO: Persist to InfluxDB
+        TODO: Clean up InfluxDB stuff so it's not terrible
         https://github.com/influxdata/influxdb-java
         Should also create scripts to provision InfluxDB (create tables, CQs etc)
 
@@ -70,5 +95,20 @@ public class TemperatureMonitor {
         */
 
     }
+
+    @Schedule(hour = "*", minute = "*", second = "*/3", info = "Every 3 second timer", timezone = "UTC")
+    private void queryTable(){
+        InfluxDB influxDB = InfluxDBFactory.connect("http://localhost:8086", "root", "root");
+        String dbName = "tempTest1";
+
+
+        influxDB.createDatabase(dbName);
+        Query query = new Query("SELECT * FROM hallway", dbName);
+        QueryResult query1 = influxDB.query(query);
+        for (QueryResult.Result r : query1.getResults()){
+            System.out.println(r.toString());
+        }
+    }
+
 
 }
